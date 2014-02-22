aa941af43922e6a89212bb8928a28434a273bb23
▟ ▖▟ ▖ <dodo.the.last@gmail.com>
2014-02-22 02:59:48 +0100

    add test for the new stuff

---
+++ tests/special-umd-commonjs-amd/input.js
@@ -0,0 +1,59 @@
+(function() {
+if(typeof define !== 'function') {
+  // NodeJS -> AMD
+  if(typeof module === 'object' && module.exports) {
+    var nodeJS = true;
+    define = function(ids, factory) {
+      factory(require, module);
+    };
+  }
+  // <script>
+  else {
+    if(typeof test === 'undefined') {
+      test = {foo: 'bar'};
+    }
+    return;
+  }
+}
+// AMD
+var deps;
+var defineFunc = function(require, module) {
+  module.exports = function(test) {
+    var mods = deps.map(function(dep) {
+      return require(dep);
+    });
+    // handle circular dependencies
+    test = test || {};
+    test.defined = test.defined || {};
+    if(test.defined[name]) {
+      return test[name];
+    }
+    test.defined[name] = true;
+    for(var i = 0; i < mods.length; ++i) {
+      mods[i](test);
+    }
+    return test;
+  };
+  module.exports.foo = 'bar';
+  module.exports(module.exports);
+};
+var tmpDefine = define;
+define = function(ids, factory) {
+  deps = (typeof ids === 'string') ? factory.slice(2) : ids.slice(2);
+  if(nodeJS) {
+    delete define;
+    return tmpDefine.apply(null, Array.prototype.slice.call(arguments, 0));
+  }
+  define = tmpDefine;
+  return define.apply(null, Array.prototype.slice.call(arguments, 0));
+};
+define([
+  'require',
+  'module',
+  './a',
+  './b',
+  './c',
+], function() {
+  defineFunc.apply(null, Array.prototype.slice.call(arguments, 0));
+});
+})();
